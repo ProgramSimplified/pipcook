102e7da19d27fef1c9a6f422eddcf3840e2e1721
杨勇 <1994101600@qq.com>
2021-01-19 14:11:33 +0800

    feat: text classification

---
+++ textClassification/output/test.js
@@ -0,0 +1,12 @@
+const predict = require('./index')
+predict('还挺便宜').then(res => {
+  console.log('test1: ', res)
+})
+
+predict('良品铺子').then(res => {
+  console.log('test2: ',res)
+})
+
+predict('电话手表').then(res => {
+  console.log('test3: ',res)
+})
